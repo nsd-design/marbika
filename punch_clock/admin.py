98d2974da218edf05fa85f51d991fff17ea95dbb
nsd-design <ibayo427@gmail.com>
2024-02-23 00:41:09 +0000

    affichage des models sur le site admin

--- punch_clock/admin.py
+++ punch_clock/admin.py
@@ -1,3 +1,16 @@
+from django.apps import apps
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
+
+# Obtenir tous les models de toutes les applications installees
+models = apps.get_models()
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+for model in models:
+    admin.site.register(model)
+
+
